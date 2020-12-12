028ba2621d735e09694fd702e65b0449942c3436
Julian Hinxlage <hinxlage.julian@web.de>
2020-12-12 20:30:18 +0100

    server test

--- src/test/serverTest.cpp
+++ src/test/serverTest.cpp
@@ -8,6 +8,70 @@
 
 using namespace sock;
 
+class Server{
+public:
+    TcpListener listener;
+    SocketHandler handler;
+    std::vector<TcpSocket> sockets;
+    uint16_t  port;
+    std::vector<char> buffer;
+    std::function<void(TcpSocket &socket, char *ptr, int bytes)> readCallback;
+    std::function<void(TcpSocket &socket)> disconnectCallback;
+    std::function<void(TcpSocket &socket)> connectCallback;
+
+    Server(uint16_t port){
+        this->port = port;
+    }
+
+    void remove(int handle){
+        handler.remove(handle);
+        for(int i = 0; i < sockets.size(); i++){
+            if(sockets[i].getHandle() == handle){
+                sockets.erase(sockets.begin() + i);
+                i--;
+            }
+        }
+    }
+
+    Error run(){
+        Error error = listener.listen(port);
+        if(error){
+            return error;
+        }
+
+        handler.add(listener.getHandle(), [&](){
+           TcpSocket socket;
+           error = listener.accept(socket);
+           if(!error){
+               sockets.push_back(socket);
+               if(connectCallback){
+                   connectCallback(socket);
+               }
+               handler.add(socket.getHandle(), [&, socket]() mutable{
+                   int bytes = 0;
+                   error = socket.readAll(buffer, bytes, 0);
+                   if(error){
+                       if(error == DISCONNECT){
+                           if(disconnectCallback){
+                               disconnectCallback(socket);
+                           }
+                       }else{
+                           std::cout << error.message << std::endl;
+                       }
+                       remove(socket.getHandle());
+                   }else{
+                       if(readCallback){
+                           readCallback(socket, buffer.data(), bytes);
+                       }
+                   }
+               });
+           }
+        });
+
+        return handler.run();
+    }
+};
+
 int main(int argc, char *argv[]){
     int port = 2000;
     for(int i = 1; i < argc; i++){
@@ -16,38 +80,11 @@ int main(int argc, char *argv[]){
         } catch (...) {}
     }
 
-    SocketHandler handler;
-    TcpListener listener;
-    Error error;
-
-
-    error = listener.listen(port);
-    if(error){
-        std::cout << error.message << std::endl;
-        return 0;
-    }
-
     int request = 1;
+    Server server(port);
 
-    handler.add(listener.getHandle(), [&](){
-        TcpSocket socket;
-        error = listener.accept(socket);
-        if(error){
-            std::cout << error.message << std::endl;
-            return;
-        }
-        handler.add(socket.getHandle(), [&, socket = socket]() mutable{
-            std::vector<char> buffer;
-            int bytes = 0;
-            error = socket.readAll(buffer, bytes, 0);
-
-            if(error){
-                std::cout << error.message << std::endl;
-                handler.remove(socket.getHandle());
-                return;
-            }
-
-            std::string msg = R"(HTTP/1.0 200 OK
+    server.readCallback = [&](TcpSocket &socket, char *ptr, int bytes){
+        std::string msg = R"(HTTP/1.0 200 OK
 Server: Test
 Content-Type: text/html
 
@@ -60,12 +97,10 @@ Content-Type: text/html
 </body>
 </html>)";
 
-            socket.write(msg.data(), msg.size());
-            handler.remove(socket.getHandle());
-        });
-    });
+        socket.write(msg.data(), msg.size());
+        server.remove(socket.getHandle());
+    };
 
-    handler.run();
-
+    server.run();
     return 0;
 }
